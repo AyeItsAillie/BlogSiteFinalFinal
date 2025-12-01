3645441fea76c8983fc6393a4e94ec1ae4a6aa74
Aillie Gove <ayeitsaillie@gmail.com>
2025-12-01 10:32:49 -0600

    Working on database

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
 app = Flask(__name__)
 app.secret_key = 'top-secret'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(25),nullable=False)
+    email = db.Column(db.String(100),nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+with app.app_context():
+    db.create_all()
+    
+
 
 
 @app.route('/')
@@ -13,14 +32,35 @@ def profile():
         username = request.form.get('username', '').strip()
         email = request.form.get('email', '').strip()
         post = request.form.get('post', '').strip()
-# Validation
-        if not username or email or not post:
+
+    # Validation
+        if not username or not email or not post:
             error = "Please fill in all required fields"
             return render_template('profileForm.html', error=error)
-        return render_template(
-            'profileSuccess.html',
+        #create new profile in database
+        try:
+            new_profile = Profile(
             username=username,
             email=email,
             post=post
-    )
+            )
+            db.session.add(new_profile)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            error = "An error occured while saving your profile. Please try again."
+            return render_template('profileForm.html', error=error)
+    
+        return render_template(
+            'profileSuccess.html',
+                username=username,
+                email=email,
+                post=post,
+            )
+            
     return render_template('profileForm.html')
+
+@app.route('/admin/profiles')
+def admin_profiles():
+    profiles = Profile.query.all()
+    return render_template('Admin_profiles.html', profiles=profiles)

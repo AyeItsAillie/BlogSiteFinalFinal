e945fbf628dd5120977a16cb49b90176cacd1c6b
AyeItsAillie <111165035+AyeItsAillie@users.noreply.github.com>
2025-12-10 18:48:33 -0600

    Merge pull request #1 from AyeItsAillie/DatabaseTesting
    Database + Edit + Delete

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
 app = Flask(__name__)
 app.secret_key = 'top-secret'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(25),nullable=False)
+    email = db.Column(db.String(100),nullable=False)
+    post = db.Column(db.String(200),nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+with app.app_context():
+    db.create_all()
+    
+
 
 
 @app.route('/')
@@ -13,14 +33,123 @@ def profile():
         username = request.form.get('username', '').strip()
         email = request.form.get('email', '').strip()
         post = request.form.get('post', '').strip()
-# Validation
-        if not username or email or not post:
+
+    # Validation
+        if not username or not email or not post:
             error = "Please fill in all required fields"
             return render_template('profileForm.html', error=error)
-        return render_template(
-            'profileSuccess.html',
+        #create new profile in database
+        try:
+            new_profile = Profile(
             username=username,
             email=email,
             post=post
-    )
+            )
+            db.session.add(new_profile)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            error = "An error occured while saving your profile. Please try again."
+            return render_template('profileForm.html', error=error)
+    
+        return render_template(
+            'profileSuccess.html',
+                username=username,
+                email=email,
+                post=post,
+            )
+            
     return render_template('profileForm.html')
+
+@app.route('/admin/profiles')
+def admin_profiles():
+    profiles = Profile.query.all()
+    return render_template('Admin_profiles.html', profiles=profiles)
+
+@app.route('/admin/profiles/deleteButton', methods=['POST'])
+def admin_profilesDeleteButton():
+    try:
+        profileId = request.form.get('profileId', '').strip()
+        profile_to_delete = Profile.query.filter_by(id=profileId).first()
+        if not profile_to_delete:
+            error = f"No profiles found with the specified id found."
+            profiles = Profile.query.all()
+            return render_template('admin_profiles.html', profiles=profile, error=error)
+        db.session.delete(profile_to_delete)
+        db.session.commit()
+        return redirect(url_for('admin_profiles'))
+    except Exception as e:
+        error = f"Error deleting profile: {str(e)}"
+        profiles = Profile.query.all()
+        return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+@app.route('/admin/profiles/edit', methods=['GET', 'POST'])
+def admin_profiles_edit():
+    if request.method == 'POST':
+        profileId = request.form.get('profileId', '')
+
+        if not profileId:
+            error = f"No profile id provided."
+            profiles = Profile.query.all()
+            return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+        profileToUpdate = Profile.query.filter_by(id=profileId).first()
+
+        if not profileToUpdate:
+            error = f"No profile found to edit with id = {profileId}."
+            profiles = Profile.query.all()
+            return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+        try:
+            profileToUpdate.username = request.form.get(
+                'username', profileToUpdate.username)
+            profileToUpdate.email = request.form.get(
+                'email', profileToUpdate.email)
+            profileToUpdate.post = request.form.get(
+                'post', profileToUpdate.post)
+            db.session.commit()
+            return redirect(url_for('admin_profiles'))
+        except Exception as e:
+            db.session.rollback()
+            error = f"Error writing to database file."
+            profiles = Profile.query.all()
+            return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+    profileId = request.args.get('profileId')
+
+    if not profileId:
+        error = f"No profile id provided."
+        profiles = Profile.query.all()
+        return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+    profileToEdit = Profile.query.filter_by(id=profileId).first()
+
+    if not profileToEdit:
+        error = f"No profile found to edit with id = {profileId}"
+        profiles = Profile.query.all()
+        return render_template('admin_profiles.html', profiles=profiles, error=error)
+
+    return render_template('profileEdit.html', profile=profileToEdit)
+
+@app.route('/admin/profiles/edit/<int:profileID>', methods=['GET', 'POST'])
+def edit_post(profileID):
+    profile = Profile.query.get_or_404(profileID)
+    
+    if request.method == 'POST':
+        username = request.form.get('username', '').strip()
+        email = request.form.get('email', '').strip()
+        post = request.form.get('post', '').strip()
+
+        if not username or not email or not post:
+            #flash('Username, Email and Post are required.', 'danger')
+            return render_template('profileEdit.html', profile=profile, page_type='input')
+
+        profile.username = username
+        profile.email = email
+        profile.post = post
+        db.session.commit()
+
+        #flash('Post updated successfully!', 'success')
+        return redirect(url_for('Admin_profiles', profileID=profileID))
+
+    return render_template('profileEdit.html', profile=profile, page_type='input')
